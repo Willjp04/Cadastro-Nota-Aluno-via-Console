97def80f8f6510d761fc6bc37ab0aecd89032af7
Willian Sostenes <sostenes655@gmail.com>
2020-09-01 00:19:46 -0300

    Criação comandos para console

---
+++ Cadastro Nota Aluno/src/cadastro/nota/aluno/CadastroNotaAluno.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadastro.nota.aluno;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author PORTOMASTER
+ */
+public class CadastroNotaAluno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o nome do Aluno"); 
+     
+    }
+    
+}
