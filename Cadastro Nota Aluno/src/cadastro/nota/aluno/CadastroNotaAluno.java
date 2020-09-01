9e289bca23d5b2284fa7adf894e9d373259f2072
Willian Sostenes <sostenes655@gmail.com>
2020-09-01 00:28:33 -0300

    Finalização Cadastro Nota Aluno no Console

--- Cadastro Nota Aluno/src/cadastro/nota/aluno/CadastroNotaAluno.java
+++ Cadastro Nota Aluno/src/cadastro/nota/aluno/CadastroNotaAluno.java
@@ -19,7 +19,11 @@ public class CadastroNotaAluno {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Digite o nome do Aluno"); 
-     
+        String nome = teclado.nextLine();
+        System.out.println("Digite a nota do Aluno");
+        float nota = teclado.nextFloat();
+        System.out.printf("A nota do Aluno é %s %.1f \n",nome,nota);
+   
     }
     
 }
